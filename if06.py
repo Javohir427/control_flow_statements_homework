f2c865bb0e9b7a4ade72c8740d89cb272ecd24ec
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-28 09:08:02 +0500

    7

--- if06.py
+++ if06.py
@@ -13,8 +13,8 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    x = 0
-    y = 0
+    x = 'there are a lot of positive numbers'
+    y = 'there are a lot of negative numbers'
     if a>0:
         x+=1
     if a<0:
@@ -28,10 +28,7 @@ def main(a,b,c):
     if c<0:
         y+=1
     
-    if x>y:
-        print ('here are a lot of positive numbers')
-    if x<y:
-        print ('there are a lot of negative numbers')
+    
 
-    return x,y
+    return (x,y)
 print(main(2,4,6))
